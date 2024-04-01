4ce3158ae153ac3cb78f8d7a018296d96e7cff0f
Scott Pinkelman <scott@scottpinkelman.com>
2024-04-01 15:37:16 -0600

    'Project setup

--- src/main/java/com/scottpinkelman/app/App.java
+++ src/main/java/com/scottpinkelman/app/App.java
@@ -1,4 +1,4 @@
-package com.mycompany.app;
+package com.scottpinkelman.app;
 
 /**
  * Hello world!
--- src/test/java/com/scottpinkelman/app/AppTest.java
+++ src/test/java/com/scottpinkelman/app/AppTest.java
@@ -1,4 +1,4 @@
-package com.mycompany.app;
+package com.scottpinkelman.app;
 
 import org.junit.jupiter.api.Test;
 
@@ -7,17 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * Unit test for simple App.
  */
-public class AppTest
+class AppTest
 {
     @Test
-    public void testAppConstructor() {
+    void testAppConstructor() {
         App app1 = new App();
         App app2 = new App();
         assertEquals(app1.getMessage(), app2.getMessage());
     }
 
     @Test
-    public void testAppMessage()
+    void testAppMessage()
     {
         App app = new App();
         assertEquals("Hello World!", app.getMessage());
